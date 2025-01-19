83473f9aa767617d8b963a44d9e3304a3984b4a9
redmoart23 <108553638+redmoart23@users.noreply.github.com>
2025-01-19 12:44:35 -0500

    feature:✨Add MongoDB connection setup and integrate with server (#3)

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+const { dbConnection } = require("./database/config");
 
 const app = express();
 
+// Environment variables
 require("dotenv").config();
 
-// TODO: database connection
-//dbConnection();
+// Database connection
+dbConnection();
 
 // CORS
 app.use(cors());
@@ -14,10 +16,12 @@ app.use(cors());
 // Body Parser
 app.use(express.json());
 
+// Routes
 app.use("/api/v1/auth", require("./routes/auth"));
 app.use("/api/v1/patients", require("./routes/patients"));
 app.use("/api/v1/observations", require("./routes/observations"));
 
+// Server
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
