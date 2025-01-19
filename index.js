e5b10d442b022dad4521ed62e4d0cc754281ffe4
red <redmoart@gmail.com>
2025-01-19 11:46:16 -0500

    :config:⚙️Add initial server setup with Express, CORS, and dotenv configuration

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('Hello World!');
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+require("dotenv").config();
+
+// TODO: database connection
+//dbConnection();
+
+// CORS
+app.use(cors());
+
+// Body Parser
+app.use(express.json());
+
+//  TODO: Create Routes
+// app.use("/api/v1/auth", require("./routes/auth"));
+// app.use("/api/v1/patients", require("./routes/patients"));
+// app.use("/api/v1/observations", require("./routes/observations"));
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
